fc62fa4e98874b06791a9094088854562ea220d8
Balam <lufemega2@gmail.com>
2023-01-29 19:38:39 -0600

    Inicie repo

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/** ============================================ typing animation ====================== ======================*/ 
+var typed = new Typed(".typing",{
+  strings:["Web Designer","Web Developer","BD Admin","Pascal Developer","Electronic Enginner"],
+  typeSpeed:100,
+  BackSpeed:60,
+  loop:true
+})  
+
+
+
+
+
+
+
+
+
